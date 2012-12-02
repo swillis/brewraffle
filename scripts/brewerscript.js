deb27b7a661d81840d0de87b7e6779b12230c7f8
Sam Willis <samueljohnwillis@gmail.com>
2012-12-02 15:47:46 +0000

    added social buttons and upgraded reset button

--- scripts/brewerscript.js
+++ scripts/brewerscript.js
@@ -8,7 +8,7 @@ brewer = brewers[Math.floor(Math.random()*brewers.length)];
 
 //localStorage["previousBrewers"] = JSON.stringify(previousBrewers)
 
-document.getElementById("the_brewer").innerHTML = brewer
+document.getElementById("the_brewer").innerHTML = brewer + "."
 document.getElementById("gearoid").style.display = 'block';
 
 //document.getElementById('previous_brewers').innerHTML = 'Previous brewers were: ' + JSON.parse(localStorage["previousBrewers"])
