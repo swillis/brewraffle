7b5c551a7c4e148dd3512655884efc39a97aee68
Sam Willis <sam@event.ly>
2012-12-06 17:33:53 +0000

    new ui and scripts

--- scripts/brewerscript.js
+++ scripts/brewerscript.js
@@ -1,5 +1,7 @@
 function chooseBrewer(){
 brewers = document.getElementById('brewers').value.split("\n")
+// Maybe new way to add to list?
+//brewers = document.getElementById('name-list').getElementsByTagName('li').value;
 brewer = brewers[Math.floor(Math.random()*brewers.length)];
 
 //previousBrewers = JSON.parse(localStorage['previousBrewers'] || "[]")
@@ -15,3 +17,27 @@ document.getElementById("gearoid").style.display = 'block';
 
 //return false;
 }
+
+// Add list items to array
+
+// $(function(){
+//     var brewers = $("#name-list li").map(function(){
+//     return $(this).text();
+// }).get().join(',');
+    
+//     alert (brewers);
+// });
+​
+
+$(document).ready(function() {
+    // Ad it
+    $("#gogogo").click(function() {
+        var newName = $("#new-name").val();
+        $('#name-list').append("<li class='name'><p>" + newName + "</p><button class='destroy'>X</button></li>");
+        $("#new-name").val('');
+    });
+    // Remove it
+    $('.destroy').live('click', function() {
+        $(this).parents("li.name").remove();
+    });
+})
