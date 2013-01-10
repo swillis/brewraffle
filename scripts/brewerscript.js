19458c60849ded5b5be04f46bffd184daab67037
Sam Willis <sam@event.ly>
2013-01-10 12:06:03 +0000

    fixed white space submission on raffler entry

--- scripts/brewerscript.js
+++ scripts/brewerscript.js
@@ -1,47 +1,53 @@
     // Brewer Script //
 
 $(document).ready(function() {
-  // Ad it
+  $addButton = $('#gogogo')
+  
+  // Add it
   $("#gogogo").click(function() {
+    if ($('#new-name').val().length == 0) {
+      return false
+    }
+
     var newName = $("#new-name").val();
-  $('#name-list').append("<li class='name'><p class='names'>" + newName + "</p><input type='button' class='destroy' value='X' /></li>");
-  $("#new-name").val('');
-  $('.raffleButton').slideDown('100');
-  $('#new-name').focus();
-  return false;
-  // $('.name').hide().fadeIn(1000);
+    $('#name-list').append("<li class='name'><p class='names'>" + newName + "</p><input type='button' class='destroy' value='X' /></li>");
+    $("#new-name").val('');
+    $('.raffleButton').slideDown('100');
+    $('#new-name').focus();
+    return false;
+    // $('.name').hide().fadeIn(1000);
   });
+
   // Remove it
   $('.destroy').live('click', function() {
-  $(this).parents("li.name").remove();
+    $(this).parents("li.name").remove();
   });
 })
 
-function chooseBrewer(){
+function chooseBrewer(event){
+  var brewList = $('#name-list');
+  var brewers = $('li');
 
-var brewList = $('#name-list');
-var brewers = $('li');
+  var brewer = brewers[Math.floor(Math.random()*brewers.length)];
 
-var brewer = brewers[Math.floor(Math.random()*brewers.length)];
+  $("#the_brewer").html($(brewer).text() + ".").hide().fadeIn(800);
 
-$("#the_brewer").html($(brewer).text() + ".").hide().fadeIn(800);
+  $('#brewerIs').hide().fadeIn(100);
+  $('#feedback').delay(500).fadeIn(400);
+  // document.getElementById("brewerIs").style.display = 'block';
+  // document.getElementById("reset").style.display = 'block';
 
-$('#brewerIs').hide().fadeIn(100);
-$('#feedback').delay(500).fadeIn(400);
-// document.getElementById("brewerIs").style.display = 'block';
-// document.getElementById("reset").style.display = 'block';
-
-$('html, body').animate({ 
-   scrollTop: $(document).height()-$(window).height()}, 
-   1400, 
-   "easeOutQuint"
-);
-
+  $('html, body').animate({ 
+     scrollTop: $(document).height()-$(window).height()}, 
+     1400, 
+     "easeOutQuint"
+  );
 }
 
-$('new-candidate-name').click(function() {
-    chooseBrewer()}
+$('new-candidate-name').click(function(e) {
+    chooseBrewer(e)}
 );
+
 
 // //previousBrewers = JSON.parse(localStorage['previousBrewers'] || "[]")
 
